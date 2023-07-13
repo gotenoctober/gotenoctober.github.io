cbd3f6e2d6ea8a64fe0c685e5831030895d3df4c
Jeff Browning <jeff@jkbrowning.com>
2023-07-13 16:01:46 -0400

    Create ContactButton

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const CONTACT_URL = "https://39s1iio27jy.typeform.com/to/xBWC3yz2";
-export const SITE_TITLE = "Hi I'm Jeff";
+export const SITE_TITLE = "Jeff Browning";
 export const SITE_DESCRIPTION =
-  "I'm a highly-experienced team manager and software developer with over 10 years of experience specializing in web and mobile applications.";
+  "The personal site of Jeff Browning, a highly-experienced team manager and software developer with over 10 years of experience specializing in web and mobile applications.";
