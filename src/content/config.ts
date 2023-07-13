1548a4b411ed2e7030eac395357a75b0fc57f7ce
Jeff Browning <jeff@jkbrowning.com>
2023-07-13 16:32:03 -0400

    Clean out unused content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,5 @@
 import { z, defineCollection } from "astro:content";
 
-const blogSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  pubDate: z.coerce.date(),
-  updatedDate: z.string().optional(),
-  heroImage: z.string().optional(),
-  badge: z.string().optional(),
-});
-
 export const projectSchema = ({ image }) =>
   z.object({
     title: z.string(),
@@ -20,16 +11,11 @@ export const projectSchema = ({ image }) =>
     order: z.number(),
   });
 
-const blogCollection = defineCollection({
-  type: "content",
-  schema: blogSchema,
-});
 const projectsCollection = defineCollection({
   type: "content",
   schema: projectSchema,
 });
 
 export const collections = {
-  blog: blogCollection,
   projects: projectsCollection,
 };
