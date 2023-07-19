7ceb2b6f546c55b9125b2dcfc2473dae271e2ec8
Jeff Browning <jeff@jkbrowning.com>
2023-07-19 15:47:39 -0400

    Reorganize content

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const CONTACT_URL = "https://39s1iio27jy.typeform.com/to/xBWC3yz2";
-export const REPO_URL =
-  "https://github.com/gotenoctober/gotenoctober.github.io";
 export const SITE_TITLE = "Jeff Browning";
 export const SITE_DESCRIPTION =
   "The personal site of Jeff Browning, a highly-experienced team manager and software developer with over 10 years of experience specializing in web and mobile applications.";
