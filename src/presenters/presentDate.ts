2844a853e9e480ac145e3f3ce54b454cbd8948e1
Jeff Browning <jeff@jkbrowning.com>
2026-02-26 17:37:51 -0500

    New landing page

--- src/presenters/presentDate.ts
+++
@@ -1,5 +0,0 @@
-import dayjs from "dayjs";
-import localizedFormat from "dayjs/plugin/localizedFormat";
-dayjs.extend(localizedFormat);
-
-export const presentDate = (date: Date | string) => dayjs(date).format("ll");
