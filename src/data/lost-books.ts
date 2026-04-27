953584b1c76a3eae98c4a745cad190b9698a70ad
Jeff Browning <jeff@jkbrowning.com>
2026-04-27 19:08:48 -0400

    Lost books landing page

---
+++ src/data/lost-books.ts
@@ -0,0 +1,7 @@
+export const APP_STORE_URL =
+  "https://apps.apple.com/us/app/hide-and-seek-lost-books/id6759506027";
+export const PLAY_STORE_URL =
+  "https://play.google.com/store/apps/details?id=com.tenoctober.hideandseeklostbooks";
+
+export const SMART_LINK_PATH = "/lost-books/download";
+export const SMART_LINK_DISPLAY = "tenoctober.com/lost-books/download";
